14b5c6cef88a3a76c5520d4ef19768deae9d0c0e
Vicente Fernández Antolín <fernandez.antolin.vicente@gmail.com>
2017-03-01 21:40:19 +0100

    Falta rematar la carga de datos

---
+++ Data/Repositories/EventRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Core;
+using Domain.Core.Respositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class EventRepository : RepositoryBase<Event>, IEventRepository
+    {
+        public EventRepository(IDataFactory dataFactory) : base(dataFactory)
+        {
+
+        }
+    }
+}
---
+++ Data/Repositories/ServiceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Core;
+using Domain.Core.Respositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class ServiceRepository : RepositoryBase<Service>, IServiceRepository
+    {
+        public ServiceRepository(IDataFactory dataFactory) : base(dataFactory)
+        {
+
+        }
+    }
+}
